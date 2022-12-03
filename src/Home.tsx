37ba006721ae5d0bf536aa69206ae0c9bfc7d57a
Johnson Rouslie Junior <jhonsonrouslie66@gmail.com>
2022-12-04 02:04:26 +0700

    Home intro done

---
+++ src/Home.tsx
@@ -0,0 +1,11 @@
+import Intro from './components/layout/home/intro';
+
+const Home = () => {
+    return (
+        <main className="flex flex-col items-center">
+            <Intro/>
+        </main>
+    );
+};
+
+export default Home;
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App';
+import Header from './components/layout/header';
+import Home from './Home';
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
     <React.StrictMode>
-        <App />
+        <Header/>
+        <Home/>
     </React.StrictMode>
 );
