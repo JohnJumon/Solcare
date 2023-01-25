a04d134c24a2f8b05a543fdafa1504a33026c691
limjesslyn <jesslynlim.0252@gmail.com>
2023-01-26 00:45:58 +0700

    add: profile info

--- src/components/layout/profile/mainProfile.tsx
+++ src/components/layout/profile/mainProfile.tsx
@@ -9,12 +9,24 @@ import { useSmartContract } from '../../../context/connection';
 import { BN } from 'bn.js';
 import axios from 'axios';
 
+interface ProfileProps {
+    firstName: string;
+    lastName: string;
+    address: string;
+    profilePicture: string;
+    gender: string;
+
+    createdAt: number;
+    isVerified: boolean;
+    isWarned: boolean;
+}
+
 const MainProfile = () => {
     const { connected, publicKey } = useWallet();
     const { smartContract } = useSmartContract();
     const [usdcBalance, setUsdcBalance] = useState(0);
     const [createdCampaign, setCreatedCampaign] = useState(0);
-    
+    const [userInfo, setUserInfo] = useState<ProfileProps>();
 
     const fetchUsdc = async () => {
         if (!connected || !publicKey) return;
@@ -29,8 +41,8 @@ const MainProfile = () => {
             );
 
         const list = await axios.get(
-                API_BASE_URL + '/v1/campaign/user/' + publicKey.toBase58()
-            );
+            API_BASE_URL + '/v1/campaign/user/' + publicKey.toBase58()
+        );
         setCreatedCampaign(list.data.data.length);
 
         if (tokenAccountInfo !== null) {
@@ -45,92 +57,135 @@ const MainProfile = () => {
         }
     };
 
-    
+    const fetchUser = async () => {
+        const userData = await axios.get(
+            `${API_BASE_URL}/v1/users/info/${publicKey?.toBase58()}`
+        );
+        if (userData.data.data != undefined) {
+            setUserInfo(userData.data.data);
+        }
+    };
 
     useEffect(() => {
         fetchUsdc();
+        fetchUser();
     }, []);
 
-    return (
-        <div className="flex flex-col">
-            <div
-                className="
-                flex flex-col
-                md:flex-row items-center shadow-[0px_4px_6px_2px_rgba(0,123,199,0.5)] p-8 rounded-[10px]"
-            >
-                <img
+    if (userInfo !== undefined) {
+        return (
+            <div className="flex flex-col">
+                <div
                     className="
-                        md:basis-1/12 w-24 h-24 rounded-full
-                        md:w-28 md:h-28"
-                    src={ProfilePlaceholder}
-                    alt="placeholder"
-                />
-                <div className="md:basis-10/12 flex flex-col items-center md:items-start my-4 md:my-0 md:mx-4">
-                    <h2
-                        className="text-lg font-bold
-                        md:text-xl"
-                    >
-                        Nama
-                    </h2>
+                    flex flex-col
+                    md:flex-row items-center shadow-[0px_4px_6px_2px_rgba(0,123,199,0.5)] p-8 rounded-[10px]"
+                >
+                    <img
+                        className="
+                            md:basis-1/12 w-24 h-24 rounded-full
+                            md:w-28 md:h-28"
+                        src={
+                            userInfo.profilePicture === ''
+                                ? ProfilePlaceholder
+                                : userInfo.profilePicture
+                        }
+                        alt="placeholder"
+                    />
+                    <div className="md:basis-10/12 flex flex-col items-center md:items-start my-4 md:my-0 md:mx-4">
+                        <h2
+                            className="text-lg font-bold
+                            md:text-xl"
+                        >
+                            {userInfo.firstName === '' ||
+                            userInfo.firstName === ''
+                                ? 'Nama'
+                                : userInfo.firstName + ' ' + userInfo.lastName}
+                        </h2>
+                        <p
+                            id="address-tag"
+                            className="text-sm
+                            md:text-base"
+                        >
+                            {connected
+                                ? publicKey?.toBase58()
+                                : 'Wallet address'}
+                        </p>
+                        <p
+                            className="text-sm
+                            md:text-base"
+                        >
+                            {userInfo.gender ? 'Pria' : 'Perempuan'}
+                        </p>
+                        <p
+                            className="text-sm
+                            md:text-base"
+                        >
+                            {userInfo.isVerified
+                                ? 'Terverifikasi'
+                                : 'Belum Terverifikasi'}
+                        </p>
+                    </div>
                     <p
-                        id="address-tag"
-                        className="text-sm
-                        md:text-base"
+                        className="text-center text-sm 
+                        md:self-start md:text-right md:basis-1/12
+                        "
                     >
-                        {connected ? publicKey?.toBase58() : 'Wallet address'}
+                        {userInfo.isWarned ? (
+                            <p className="text-red-600 text-bold">
+                                Diperingati
+                            </p>
+                        ) : (
+                            <p className="text-green-600 text-bold">
+                                Belum Diperingati
+                            </p>
+                        )}
                     </p>
                 </div>
-                <p
-                    className="text-center text-xs 
-                    md:self-start md:text-right md:basis-1/12 md:text-sm
-                    "
-                >
-                    Status
+                <div className="divider"></div>
+                <p className="text-left font-bold text-xs sm:text-lg mb-2 sm:mb-4">
+                    Saldo Saat Ini
                 </p>
-            </div>
-            <div className="divider"></div>
-            <p className="text-left font-bold text-xs sm:text-lg mb-2 sm:mb-4">
-                Saldo Saat Ini
-            </p>
-            <div className="font-bold shadow-[0px_4px_6px_2px_rgba(0,123,199,0.5)] p-4 rounded-[10px] mb-6 text-center">
-                <p className="text-[#007BC7] text-3xl sm:text-6xl">
-                    {usdcBalance.toLocaleString().replaceAll(',', '.')}
-                    <span className="text-3xl max-[639px]:hidden">USDC</span>
-                </p>
-                <p className="sm:hidden text-[15px] text-[#007BC7]">USDC</p>
-            </div>
-            <div className="grid grid-cols-1 gap-6 font-bold text-xs sm:text-lg md:grid-cols-2 md:grid-rows-2 md:gap-4 items-center">
-                <div className="col-span-1">
-                    <p>Total Donasi</p>
-                    <div className="text-3xl">
-                        <p>xx</p>
-                        <p className="text-[15px] leading-none">USDC</p>
-                    </div>
+                <div className="font-bold shadow-[0px_4px_6px_2px_rgba(0,123,199,0.5)] p-4 rounded-[10px] mb-6 text-center">
+                    <p className="text-[#007BC7] text-3xl sm:text-6xl">
+                        {usdcBalance.toLocaleString().replaceAll(',', '.')}
+                        <span className="text-3xl max-[639px]:hidden">
+                            USDC
+                        </span>
+                    </p>
+                    <p className="sm:hidden text-[15px] text-[#007BC7]">USDC</p>
                 </div>
-                <div className="col-span-1">
-                    <p>Total Pendapatan</p>
-                    <div className="text-3xl">
-                        <p>xx</p>
-                        <p className="text-[15px] leading-none">USDC</p>
+                <div className="grid grid-cols-1 gap-6 font-bold text-xs sm:text-lg md:grid-cols-2 md:grid-rows-2 md:gap-4 items-center">
+                    <div className="col-span-1">
+                        <p>Total Donasi</p>
+                        <div className="text-3xl">
+                            <p>xx</p>
+                            <p className="text-[15px] leading-none">USDC</p>
+                        </div>
                     </div>
-                </div>
-                <div className="col-span-1">
-                    <p>Campaign Yang Didonasi</p>
-                    <div className="text-3xl">
-                        <p>xx</p>
-                        <p className="text-[15px] leading-none">Campaign</p>
+                    <div className="col-span-1">
+                        <p>Total Pendapatan</p>
+                        <div className="text-3xl">
+                            <p>xx</p>
+                            <p className="text-[15px] leading-none">USDC</p>
+                        </div>
                     </div>
-                </div>
-                <div className="col-span-1">
-                    <p>Campaign Yang Dibuat</p>
-                    <div className="text-3xl">
-                        <p>{createdCampaign}</p>
-                        <p className="text-[15px] leading-none">Campaign</p>
+                    <div className="col-span-1">
+                        <p>Campaign Yang Didonasi</p>
+                        <div className="text-3xl">
+                            <p>xx</p>
+                            <p className="text-[15px] leading-none">Campaign</p>
+                        </div>
+                    </div>
+                    <div className="col-span-1">
+                        <p>Campaign Yang Dibuat</p>
+                        <div className="text-3xl">
+                            <p>{createdCampaign}</p>
+                            <p className="text-[15px] leading-none">Campaign</p>
+                        </div>
                     </div>
                 </div>
             </div>
-        </div>
-    );
+        );
+    }
 };
 
 export default MainProfile;
