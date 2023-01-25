1e9a83055d8f51dd5fdc3d95b54ae47ceaba92dd
limjesslyn <jesslynlim.0252@gmail.com>
2023-01-26 02:40:03 +0700

    setInfo axios error

--- src/components/layout/profile/setInfo.tsx
+++ src/components/layout/profile/setInfo.tsx
@@ -1,4 +1,76 @@
+import { useWallet } from '@solana/wallet-adapter-react';
+import axios from 'axios';
+import { useState } from 'react';
+import { API_BASE_URL } from '../../../utils';
+
 const SetInfo = () => {
+    const { publicKey } = useWallet();
+
+    const [input, setInput] = useState<{ [string: string]: any }>({
+        email: '',
+        firstName: '',
+        lastName: '',
+        gender: false,
+    });
+
+    const handleInputChange = (e: any) => {
+        const target = e.target;
+        const name = target.name;
+        
+        let genderValue = true;
+
+        setInput((state) => {            
+            const newState = {
+                ...state,
+            };
+            newState[name] = name;
+            if(newState[name]  === 'gender') {
+                target.value === 'Wanita' ? genderValue = false : genderValue = true;
+                newState[name] = genderValue;
+            }
+            else {
+                newState[name] = target.value;
+            }
+            
+            console.log(newState);
+
+            return newState;
+        });
+    };
+
+    const submitInfo = async () => {
+        let token = localStorage.getItem('token')
+        
+        const headers = {
+            'Authorization': `Bearer ${token}`
+        }
+        try {
+            const content = {
+                email: input.email,
+                firstName: input.firstName,
+                lastName: input.lastName,
+                gender: input.gender,
+            }
+            // console.log(content);
+            
+            const resp = await axios.put(
+                API_BASE_URL + '/v1/users/info/' + publicKey?.toBase58(), content, { headers }
+            );
+            console.log(resp.data.json);
+            
+            if (resp.data.status !== 200) {
+                // toastDone();
+                // toast.error(`Campaign gagal dibuat!`);
+                console.log('error');
+
+                return;
+            }
+        } catch(e) {
+            console.log(e);
+            return;
+        }
+    };
+
     return (
         <div className="flex flex-col">
             <p className="font-bold text-xs mt-8 md:text-lg">Data Diri</p>
@@ -9,6 +81,9 @@ const SetInfo = () => {
                         text-xs p-2 w-full rounded-[5px] border border-gray-300 hover:bg-gray-100 hover:text-gray-700 focus:outline-none
                         md:text-xl md:p-4 md:rounded-[10px]"
                     type="text"
+                    name='firstName'
+                    onChange={handleInputChange}
+                    // value={input.firstName}
                 />
             </div>
             <div className="mt-2">
@@ -18,6 +93,9 @@ const SetInfo = () => {
                         text-xs p-2 w-full rounded-[5px] border border-gray-300 hover:bg-gray-100 hover:text-gray-700 focus:outline-none
                         md:text-xl md:p-4 md:rounded-[10px]"
                     type="text"
+                    name='lastName'
+                    onChange={handleInputChange}
+                    // value={input.lastName}
                 />
             </div>
             <div className="mt-2">
@@ -27,6 +105,9 @@ const SetInfo = () => {
                         text-xs p-2 w-full rounded-[5px] border border-gray-300 hover:bg-gray-100 hover:text-gray-700 focus:outline-none
                         md:text-xl md:p-4 md:rounded-[10px]"
                     type="text"
+                    name='email'
+                    onChange={handleInputChange}
+                    // value={input.email}
                 />
             </div>
             <div className="mt-2">
@@ -37,7 +118,8 @@ const SetInfo = () => {
                             id="m-radio"
                             type="radio"
                             value="Pria"
-                            name="gender-radio-group"
+                            name="gender"
+                            onChange={handleInputChange}
                             className="w-2 h-2 md:w-4 md:h-4 text-[#007BC7] bg-gray-100 border-gray-300"
                         />
                         <label
@@ -52,7 +134,8 @@ const SetInfo = () => {
                             id="f-radio"
                             type="radio"
                             value="Wanita"
-                            name="gender-radio-group"
+                            name="gender"
+                            onChange={handleInputChange}
                             className="w-2 h-2 md:w-4 md:h-4 text-[#007BC7] bg-gray-100 border-gray-300"
                         />
                         <label
@@ -68,6 +151,7 @@ const SetInfo = () => {
                 className="
                     mt-4 self-end bg-[#007BC7] text-xs w-full p-2 border border-[2px] border-[#007BC7] text-white font-bold rounded-[5px]
                     md:text-xl md:p-4 md:rounded-[10px]"
+                onClick={submitInfo}
             >
                 Simpan Data
             </button>
