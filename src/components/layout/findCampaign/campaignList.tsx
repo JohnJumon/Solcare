3fb86385ec2b41a7b964c413225005b0bf362f91
JohnJumon <jhonsonrouslie66@gmail.com>
2023-01-27 01:36:45 +0700

    add load more button

--- src/components/layout/findCampaign/campaignList.tsx
+++ src/components/layout/findCampaign/campaignList.tsx
@@ -26,6 +26,8 @@ const CampaignList = () => {
 
     const [initializing, setInitializing] = useState(true);
 
+    const [offset, setOffset] = useState(0);
+
     const { smartContract } = useSmartContract();
 
     let category = searchParams.get('categoryId');
@@ -45,8 +47,8 @@ const CampaignList = () => {
     const fetchAllCampaign = async () => {
         const response = await axios.get(
             API_BASE_URL +
-                '/v1/campaign?' +
-                `categoryId=${category}&order=${filter}&search=${search}`
+            '/v1/campaign?' +
+            `categoryId=${category}&order=${filter}&search=${search}`
         );
         const responseData = response.data.data;
         const campaigns: CampaignInfo[] = [];
@@ -80,6 +82,7 @@ const CampaignList = () => {
 
         setInitializing(false);
         setAllCampaigns(campaigns);
+        setOffset(offset + 10);
     };
 
     const location = useLocation();
@@ -96,17 +99,25 @@ const CampaignList = () => {
         allCampaigns?.length === 0 ? (
             <p>Belum ada campaign</p>
         ) : (
-            <div className="grid grid-cols-1 gap-6 mt-6 sm:grid-cols-2 md:grid-cols-3 xl:grid-cols-3">
-                {allCampaigns?.map((campaign) => {
-                    return (
-                        <div className="gap-6 mt-6" key={campaign.address}>
-                            <CampaignCard
-                                key={campaign.address}
-                                campaign={campaign}
-                            />
-                        </div>
-                    );
-                })}
+            <div className='flex flex-col items-center'>
+                <div className="grid grid-cols-1 gap-6 mt-6 sm:grid-cols-2 md:grid-cols-3 xl:grid-cols-3">
+                    {allCampaigns?.map((campaign) => {
+                        return (
+                            <div className="gap-6 mt-6" key={campaign.address}>
+                                <CampaignCard
+                                    key={campaign.address}
+                                    campaign={campaign}
+                                />
+                            </div>
+                        );
+                    })}
+                </div>
+                <button className="mt-8 stroke-black hover:stroke-[rgba(0,123,199,1)]" onClick={fetchAllCampaign}>
+                    <svg width="32" height="32" viewBox="0 0 32 32" fill="none" xmlns="http://www.w3.org/2000/svg">
+                        <path d="M26 16L16 26L6 16" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
+                        <path d="M26 6L16 16L6 6" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
+                    </svg>
+                </button>
             </div>
         );
     return <>{content}</>;
