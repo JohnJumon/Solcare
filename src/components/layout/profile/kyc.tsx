2be641b97b067c84be180fdcd69ea1d37431c1e5
JohnJumon <jhonsonrouslie66@gmail.com>
2023-02-20 16:27:00 +0700

    add request kyc features but still have some errors

--- src/components/layout/profile/kyc.tsx
+++ src/components/layout/profile/kyc.tsx
@@ -1,6 +1,95 @@
 import Thumbnail from '../../../image/placeholder.svg';
+import { useState } from 'react';
+import { toast, Id } from 'react-toastify';
+import axios from 'axios';
+import { API_BASE_URL } from '../../../utils';
+const KYC = (props: any) => {
+    const [input, setInput] = useState<{ [string: string]: any }>({
+        nik: null,
+        idCard: null,
+        face: null,
+        faceWithIdCard: null,
+    });
 
-const KYC = () => {
+    const [uploadedPic, setUploadedPic] = useState<{ [string: string]: any}> ({
+        idCard: '',
+        face: '',
+        faceWithIdCard: '',
+    });
+
+    const handleInputChange = (e: any) => {
+        const target = e.target;
+        const name = target.name;
+        console.log(target.files)
+        setInput((state) => {
+            const newState = {
+                ...state,
+            };
+            newState[name] = name === 'nik' ? target.value : target.files[0];
+            return newState;
+        });
+
+        if (name === 'idCard' || name === 'face' || name === 'faceWithIdCard') {
+            let file = target.files[0];
+            if (file.type === 'image/png' || file.type === 'image/jpeg') {
+                setUploadedPic((state)=>{
+                    const newState = {
+                        ...state,
+                    }
+                    newState[name] = file.name
+                    return newState;
+                });
+            } else {
+                toast.error('Format file invalid');
+            }
+        }
+    };
+
+    const submitInfo = async () => {
+        let token = localStorage.getItem('token');
+
+        const headers = {
+            Authorization: `Bearer ${token}`,
+        };
+        if (
+            input.nik === '' ||
+            input.idCard === '' ||
+            input.face === '' ||
+            input.faceWithIdCard === ''
+        ) {
+            return toast.error(
+                'Mohon isi seluruh data pada form terlebih dahulu'
+            );
+        }
+        try {
+            const content = {
+                nik: input.nik,
+                idCard: input.idCard,
+                face: input.face,
+                faceWithIdCard: input.faceWithIdCard,
+            };
+            console.log(content)
+            const resp = await axios.put(
+                API_BASE_URL + '/v1/users/kyc',
+                content,
+                { headers }
+            );
+            if (resp.data.status !== 200) {
+                toast.error(
+                    `Data Gagal Disimpan! Pastikan data baru tidak sama dengan data sebelumnya`
+                );
+                return;
+            }
+            toast.success('Data Berhasil Tersimpan');
+        } catch (e) {
+            console.log(e);
+            toast.error(
+                `Data Gagal Disimpan! Pastikan data sudah ditulis dengan format yang benar`
+            );
+            return;
+        }
+    };
+
     return (
         <div className="flex flex-col">
             <p className="font-bold text-xs md:text-lg">KYC</p>
@@ -11,6 +100,8 @@ const KYC = () => {
                         text-xs p-2 w-full rounded-[5px] border border-gray-300 hover:bg-gray-100 hover:text-gray-700 focus:outline-none
                         md:text-xl md:p-4 md:rounded-[10px]"
                     type="text"
+                    name='nik'
+                    onChange={handleInputChange}
                 />
             </div>
             <div className="mt-4 flex flex-col md:flex-row gap-4">
@@ -21,7 +112,7 @@ const KYC = () => {
                         KTP
                     </p>
                     <label
-                        htmlFor="dropzone-file"
+                        htmlFor="dropzone-file-1"
                         className={`hover:brightness-90 shrink-0 md:h-40 md:w-40 h-28 w-full bg-no-repeat bg-cover bg-center flex flex-col items-center justify-center border-2 border-gray-300 border-dashed rounded-[10px] cursor-pointer`}
                     >
                         <div className="w-full h-full flex flex-col items-center justify-center rounded-[10px]">
@@ -41,10 +132,20 @@ const KYC = () => {
                                 ></path>
                             </svg>
                             <input
-                                id="dropzone-file"
+                                id="dropzone-file-1"
                                 type="file"
                                 className="hidden"
+                                name='idCard'
+                                accept="image/png, image/jpeg"
+                                value={input.idCard?.webkitRelativePath}
+                                onChange={handleInputChange}
                             />
+                             <p
+                                id="address-tag-3"
+                                className="px-4 text-center text-xs md:text-base"
+                            >
+                                {uploadedPic.idCard}
+                            </p>
                         </div>
                     </label>
                 </div>
@@ -55,7 +156,7 @@ const KYC = () => {
                         Foto Diri
                     </p>
                     <label
-                        htmlFor="dropzone-file"
+                        htmlFor="dropzone-file-2"
                         className={`hover:brightness-90 shrink-0 md:h-40 md:w-40 h-28 w-full bg-no-repeat bg-cover bg-center flex flex-col items-center justify-center border-2 border-gray-300 border-dashed rounded-[10px] cursor-pointer`}
                     >
                         <div className="w-full h-full flex flex-col items-center justify-center rounded-[10px]">
@@ -75,10 +176,20 @@ const KYC = () => {
                                 ></path>
                             </svg>
                             <input
-                                id="dropzone-file"
+                                id="dropzone-file-2"
                                 type="file"
                                 className="hidden"
+                                name='face'
+                                accept="image/png, image/jpeg"
+                                value={input.face?.webkitRelativePath}
+                                onChange={handleInputChange}
                             />
+                             <p
+                                id="address-tag-3"
+                                className="px-4 text-center text-xs md:text-base"
+                            >
+                                {uploadedPic.face}
+                            </p>
                         </div>
                     </label>
                 </div>
@@ -89,7 +200,7 @@ const KYC = () => {
                         KTP & Foto Diri
                     </p>
                     <label
-                        htmlFor="dropzone-file"
+                        htmlFor="dropzone-file-3"
                         className={`hover:brightness-90 shrink-0 md:h-40 md:w-40 h-28 w-full bg-no-repeat bg-cover bg-center flex flex-col items-center justify-center border-2 border-gray-300 border-dashed rounded-[10px] cursor-pointer`}
                     >
                         <div className="w-full h-full flex flex-col items-center justify-center rounded-[10px]">
@@ -109,10 +220,20 @@ const KYC = () => {
                                 ></path>
                             </svg>
                             <input
-                                id="dropzone-file"
+                                id="dropzone-file-3"
                                 type="file"
                                 className="hidden"
+                                name='faceWithIdCard'
+                                accept="image/png, image/jpeg"
+                                value={input.faceWithIdCard?.webkitRelativePath}
+                                onChange={handleInputChange}
                             />
+                            <p
+                                id="address-tag-3"
+                                className="px-4 text-center text-xs md:text-base"
+                            >
+                                {uploadedPic.faceWithIdCard}
+                            </p>
                         </div>
                     </label>
                 </div>
@@ -122,6 +243,7 @@ const KYC = () => {
                     className="
                     basis-1/2 self-end bg-[#007BC7] text-xs w-full p-2 border border-[2px] border-[#007BC7] text-white font-bold rounded-[5px]
                     md:text-xl md:p-4 md:rounded-[10px]"
+                    onClick={submitInfo}
                 >
                     Verifikasi
                 </button>
