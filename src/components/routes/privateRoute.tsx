b97e96c1afd18de09d1dc30c76e63b7d9e715c1e
limjesslyn <jesslynlim.0252@gmail.com>
2023-03-08 00:15:56 +0700

    add: show KYC item

--- src/components/routes/privateRoute.tsx
+++ src/components/routes/privateRoute.tsx
@@ -1,7 +1,7 @@
 import { useWallet } from '@solana/wallet-adapter-react';
 import { WalletMultiButton } from '@solana/wallet-adapter-react-ui';
 import { decodeJwt } from 'jose';
-import { useEffect, useState } from 'react'
+import { useEffect, useState } from 'react';
 import { API_BASE_URL } from '../../utils';
 import axios from 'axios';
 import base58 from 'bs58';
@@ -9,53 +9,51 @@ import { now } from '../../utils';
 import { LOGIN_MESSAGE } from '../../utils';
 import { publicKey } from '@project-serum/anchor/dist/cjs/utils';
 const PrivateRoute = (props: any) => {
-    const { connected, publicKey, disconnecting } =
-        useWallet();
+    const { connected, publicKey, disconnecting } = useWallet();
     const [isAdmin, setIsAdmin] = useState(false);
 
     useEffect(() => {
         if (connected) {
-            const tokenString = localStorage.getItem('token')
+            const tokenString = localStorage.getItem('token');
             if (tokenString) {
-                const tokenDetail = decodeJwt(tokenString)
+                const tokenDetail = decodeJwt(tokenString);
                 if (typeof tokenDetail.isAdmin == 'boolean') {
-                    setIsAdmin(tokenDetail.isAdmin)
+                    setIsAdmin(tokenDetail.isAdmin);
                 }
             }
         }
-    },[connected, disconnecting, publicKey, ])
-
-    
+    }, [connected, disconnecting, publicKey]);
 
     return (
         <>
-            {
-                props.forAdmin ?
-                    (
-                        connected ?
-                            (
-                                isAdmin ? props.children :
-                                    <div className="min-h-[70vh] flex flex-wrap flex-col content-center justify-center bg-gray-100">
-                                        <p className="text-xl text-center px-8">
-                                            Anda Tidak Punya Akses di Halaman Ini
-                                        </p>
-                                    </div>
-                            ) :
-                            <div className="min-h-[70vh] flex flex-wrap flex-col content-center justify-center bg-gray-100">
-                                <p className="text-xl text-center px-8">
-                                    Silahkan Hubungkan Wallet Terlebih Dahulu
-                                </p>
-                                <WalletMultiButton
-                                    style={{ backgroundColor: '#007BC7' }}
-                                    className="btn !text-xs capitalize !bg-[#007BC7] !rounded-[5px] !h-12 xl:!h-16 !w-[11rem] xl:!rounded-[10px] xl:!w-[22rem] xl:!text-xl mx-auto mt-5"
-                                >
-                                    Hubungkan Wallet
-                                </WalletMultiButton>
-                            </div>
-                    ) :
-                    (
-                        connected ? props.children :
+            {props.forAdmin ? (
+                connected ? (
+                    isAdmin ? (
+                        props.children
+                    ) : (
                         <div className="min-h-[70vh] flex flex-wrap flex-col content-center justify-center bg-gray-100">
+                            <p className="text-xl text-center px-8">
+                                Anda Tidak Punya Akses di Halaman Ini
+                            </p>
+                        </div>
+                    )
+                ) : (
+                    <div className="min-h-[70vh] flex flex-wrap flex-col content-center justify-center bg-gray-100">
+                        <p className="text-xl text-center px-8">
+                            Silahkan Hubungkan Wallet Terlebih Dahulu
+                        </p>
+                        <WalletMultiButton
+                            style={{ backgroundColor: '#007BC7' }}
+                            className="btn !text-xs capitalize !bg-[#007BC7] !rounded-[5px] !h-12 xl:!h-16 !w-[11rem] xl:!rounded-[10px] xl:!w-[22rem] xl:!text-xl mx-auto mt-5"
+                        >
+                            Hubungkan Wallet
+                        </WalletMultiButton>
+                    </div>
+                )
+            ) : connected ? (
+                props.children
+            ) : (
+                <div className="min-h-[70vh] flex flex-wrap flex-col content-center justify-center bg-gray-100">
                     <p className="text-xl text-center px-8">
                         Silahkan Hubungkan Wallet Terlebih Dahulu
                     </p>
@@ -66,10 +64,9 @@ const PrivateRoute = (props: any) => {
                         Hubungkan Wallet
                     </WalletMultiButton>
                 </div>
-                    )
-            }
+            )}
         </>
-    )
+    );
 };
 
 export default PrivateRoute;
